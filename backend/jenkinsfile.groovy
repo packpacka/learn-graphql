de7ee76444a382dae0cd6a7475e39fa4c97a74ec
Evgeny Chirko <zhenya_chirko@mail.ru>
2020-07-02 22:56:02 +0300

    Поправлен jenkinsfile

--- backend/jenkinsfile.groovy
+++ backend/jenkinsfile.groovy
@@ -2,40 +2,27 @@ pipeline {
   agent any
 
   parameters {
-    number(defaultValue: 5000, description: 'Порт API', name: 'port')
+    string(defaultValue: "5000", description: 'Порт API', name: 'port', trim: true)
   }
 
 
   stages {
-    stage('Build docker image') {
+    stage('Build and run') {
       steps {
-        script {
-          withCredentials([
-            usernamePassword(credentialsId: "registry-csssr-cloud", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
-          ]) {
-            sh """
-              image=learn-graphql:${GIT_BRANCH}
-              echo "Building and pushing image \$image"
-              docker kill \$image
-              docker build --network host -f Dockerfile . -t \$image
-              echo "Docker image \$image successfully built"
-              echo "Docker image \$image successfully pushed"
-            """
-          }
-        }
-      }
-    }
-
-    stage('Run Container') {
-      steps {
-        script {
-          sh """#!/bin/bash
-            source ~/.bashrc
-            docker run -p localhost:5000:${params.port}/tcp  learn-graphql:${GIT_BRANCH}
-          """
-        }
+         sh """
+           image=learn-graphql_${GIT_BRANCH}
+           echo "Remove old image \$image"
+           [ "\$(docker images -qa --filter=reference=\${image})" ] && docker rmi \$image
+           echo "Building image \$image"
+           docker build --network host -t \$image -f ./backend/Dockerfile ./backend 
+           echo "Run new container \$image"
+           docker run -d --network host -p 0.0.0.0:5000:5000/tcp \$image
+         """
       }
     }
   }
 }
 
+//  echo "Remove old container \$image"
+// [ "\$(docker ps -a -q --filter ancestor=\$image --format="{{.ID}}")" ] && docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=\$image --format="{{.ID}}"))
+          
